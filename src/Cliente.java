5c9fb07892f69a7fad2e850e4d066536804bde8c
Lucas <luca5_andrade@outlook.com>
2024-12-30 20:34:43 -0300

    Modulo 7 sem comentario

---
+++ src/Cliente.java
@@ -0,0 +1,62 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Cliente {
+    private String nome;
+    private int idade;
+    private String email;
+    private String telefone;
+
+    public Cliente(String nome, int idade, String email, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Informaçoes do cliente");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Email: " + this.email);
+        System.out.println("Telefone " + this.telefone);
+    }
+
+    public String cumprimetarCliente() {
+        return "Olá, " + this.nome + "!";
+    }
+}
